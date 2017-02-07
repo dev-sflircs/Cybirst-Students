d071184f4c1b83b273ce7cf29f8f63285b0b8d36
devsflircs <sflircs@gmail.com>
2017-02-08 06:54:37 +0700

    complete UI

---
+++ Cybirst/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cybirst.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
---
+++ Cybirst/Controllers/EpagesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cybirst.Controllers
+{
+    public class EpagesController : Controller
+    {
+        // GET: Epages
+        public ActionResult Notfound()
+        {
+            return View();
+        }
+
+        public ActionResult InternalError()
+        {
+            return View();
+        }
+    }
+}
