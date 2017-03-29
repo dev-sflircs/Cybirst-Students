1af885fc47c4bb276b73bc5304237e70dd0e59d2
Nguyên Minh Dương <Nguyn Minh Dng>
2017-03-30 01:09:37 +0700

    complete 80/100 landing page without image

--- Cybirst/Controllers/LandingPageController.cs
+++ Cybirst/Controllers/LandingPageController.cs
@@ -3,64 +3,143 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Cybirst.Models;
 
 namespace Cybirst.Controllers
 {
-    public class Instructor
-    {
-        public string Name { get; set; }
-        public string Avatar { get; set; }
-        public string Description { get; set; }
-        public string[] Teaching { get; set; }
-    } 
-
     public class LandingPageController : Controller
     {
+        private DataClasses1DataContext dataContext;
+        private string nameIntructor;
+        private string nameTech;
 
-        List<Instructor> _Instructors = new List<Instructor>()
+        public LandingPageController()
         {
-            new Instructor()
+            dataContext = new DataClasses1DataContext();
+        }
+
+        [ChildActionOnly]
+        public ActionResult HundredsFreeVideos()
+        {
+            return PartialView("HundredsFreeVideos");
+        }
+
+        [ChildActionOnly]
+        public ActionResult HomeFirst()
+        {
+            List<Technology> list_Technologies = new List<Technology>();
+            var query = (from c in dataContext.Technologies select c).ToList();
+            foreach (Technology tech in query)
             {
-                Name = "Tran Duy Hung",
-                Avatar = @"~/Images/956631.jpeg",
-                Description = "Working at Facebook on React. Creator of Redux. 1",
-                Teaching = new string[]{"Reactjs", "Reduct" }
-            },
-            new Instructor()
-            {
-                Name = "Tran Duy Hung",
-                Avatar = @"~/Images/9VsY9i09.jpeg",
-                Description = "Working at Facebook on React. Creator of Redux. 2",
-                Teaching = new string[]{"Reactjs", "Angular" }
-            },
-            new Instructor()
-            {
-                Name = "Tran Duy Hung",
-                Avatar = @"~/Images/9VsY9i09.jpeg",
-                Description = "Working at Facebook on React. Creator of Redux. 3",
-                Teaching = new string[]{"Reactjs", "Angular2" }
-            },
-            new Instructor()
-            {
-                Name = "Tran Duy Hung",
-                Avatar = @"~/Images/9VsY9i09.jpeg",
-                Description = "Working at Facebook on React. Creator of Redux. 4",
-                Teaching = new string[]{"Reactjs"}
+                list_Technologies.Add(
+                   new Technology
+                   {
+                       Name = tech.Name,
+                       Image = tech.Image
+                   }
+                );
             }
-        };
+            ViewBag.Technologies = list_Technologies;
+
+            return PartialView("HomeFirst");
+        }
 
         [ChildActionOnly]
         public ActionResult TaughtByLeaders()
         {
-            ViewBag.Instructors = _Instructors;
-
+            List<Instructor> list_Instructors = new List<Instructor>();
+            Dictionary<string, List<string>> dict_TechIns = new Dictionary<string, List<string>>();
+            var intructorQuery = (from i in dataContext.Instructors select i).ToList();
+            foreach (var temp in intructorQuery)
+            {
+                list_Instructors.Add(new Instructor()
+                {
+                    ID = temp.ID,
+                    UID = temp.UID,
+                    FirstName = temp.FirstName,
+                    LastName = temp.LastName,
+                    Intro = temp.Intro,
+                    Google = temp.Google,
+                    Twitter = temp.Twitter,
+                    Blog = temp.Blog,
+                    Email = temp.Email,
+                    Password = temp.Password,
+                    Phone = temp.Phone,
+                    Address = temp.Address,
+                    Avatar = temp.Avatar,
+                    LargeAvatar = temp.LargeAvatar,
+                    MediumAvatar = temp.MediumAvatar,
+                    LastUpdate = temp.LastUpdate
+                });
+                var z = dataContext.Technologies.Where(t =>
+                                               dataContext.Courses.Where(u =>
+                                               dataContext.Lessons.Where(crtu => crtu.InstructorID == temp.ID)
+                                               .Select(crtu => crtu.CourseID)
+                                               .Contains(u.ID))
+                                               .Select(te => te.ID)
+                                               .Contains(t.ID));
+                List<string> listTech = new List<string>();
+                foreach (var tech in z)
+                {
+                    listTech.Add(tech.Name);
+                }
+                dict_TechIns.Add(temp.ID.ToString(), listTech);
+            }
+            ViewBag.Instructors = list_Instructors;
+            ViewBag.TechsTaughtByInstructors = dict_TechIns;
+            
             return PartialView("TaughtByLeaders");
         }
 
         [ChildActionOnly]
         public ActionResult CoursesInProduction()
         {
-            ViewBag.Instructors = _Instructors;
+            var qListCourse = (from c in dataContext.Courses select c).ToList();
+            List<CoursesInProduction> listCourseInProduct = new List<CoursesInProduction>();
+            
+            List<Course> listCourse = new List<Course>();
+            foreach (var lc in qListCourse)
+            {
+                listCourse.Add(new Course()
+                {
+                    ID = lc.ID,
+                    TeachnologyID = lc.TeachnologyID,
+                    Name = lc.Name,
+                    Image = lc.Image,
+                    Intro = lc.Intro,
+                });
+            }
+
+            foreach (Course course in listCourse)
+            {
+
+                var query = (dataContext.Instructors.Where(ins =>
+                                   dataContext.Lessons.Where(les =>
+                                   les.CourseID == course.ID)
+                                  .Select(i => i.InstructorID)
+                                  .Contains(ins.ID)
+                     )).ToList();
+                foreach (var instructor in query)
+                {
+                    nameIntructor = instructor.FirstName + instructor.LastName;
+                }
+
+                var query2 = (dataContext.Technologies.Where(c => c.ID == course.TeachnologyID)).ToList();
+                foreach (var tech in query2)
+                {
+                    nameTech = tech.Name;
+                }
+                listCourseInProduct.Add(
+                    new CoursesInProduction()
+                    {
+                        Image = course.Image,
+                        Name = course.Name,
+                        Technology = nameTech,
+                        Instructor = nameIntructor,
+                    }
+                );
+            }
+            ViewBag.Courses = listCourseInProduct;
 
             return PartialView("CoursesInProduction");
         }
@@ -68,7 +147,7 @@ namespace Cybirst.Controllers
         [ChildActionOnly]
         public ActionResult WhatDoYouWantToLearn()
         {
-            ViewBag.Instructors = _Instructors;
+            //ViewBag.Instructors = _Instructors;
 
             return PartialView("WhatDoYouWantToLearn");
         }
@@ -76,9 +155,9 @@ namespace Cybirst.Controllers
         [ChildActionOnly]
         public ActionResult BlockQuote()
         {
-            ViewBag.Instructors = _Instructors;
+            //ViewBag.Instructors = _Instructors;
 
-            return PartialView("WhatDoYouWantToLearn");
+            return PartialView("BlockQuote");
         }
 
         // GET: LandingPage
