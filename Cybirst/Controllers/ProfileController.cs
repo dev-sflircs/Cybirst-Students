f4b1e89eb8268490b8ef317ab5c5c37f7580dae4
khanhlq <khanh96le@gmail.com>
2017-05-11 14:47:16 +0700

    Dump data to Profile View

--- Cybirst/Controllers/ProfileController.cs
+++ Cybirst/Controllers/ProfileController.cs
@@ -11,6 +11,7 @@ namespace Cybirst.Controllers
     {
         private DataClasses1DataContext dataContext;
         private DataAdapter dataAdapter;
+
         public ProfileController()
         {
             dataContext = new DataClasses1DataContext();
@@ -20,9 +21,24 @@ namespace Cybirst.Controllers
         // GET: Profile
         public ActionResult Index(string uid)
         {
-            Student st = dataContext.Students.Where(x => x.UID == uid).FirstOrDefault();
-            ViewBag.Student = dataAdapter.Chain(st);
-            return View(st);
+            Student student = dataContext.Students.Where(x => x.ID == ID).FirstOrDefault();
+            List<Enrollment> enrollments = student.Enrollments.ToList();
+            List<Course> courses = new List<Course>();
+            int minutes_watched = 0;
+            int lessons_watched = 0;
+            foreach (Enrollment enrollment in enrollments) {
+                Course course = enrollment.Course;
+                courses.Add(course);
+                lessons_watched = lessons_watched + enrollment.OrderState; // compute total lessons student watched in each enrollment
+                for (int i = 0; i < enrollment.OrderState; i++) {
+                    minutes_watched = minutes_watched + enrollment.Course.Lessons[i].EstimatedTime; // compute total minutes student watched in each enrollment
+                }
+            }
+            minutes_watched = minutes_watched / 60;
+            ViewBag.minutes_watched = minutes_watched;
+            ViewBag.courses = courses;
+            ViewBag.lessons_watched = lessons_watched;
+            return View(student);
         }
     }
 }
